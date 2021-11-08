aeaa6210cb82a78d9c1146bf2a35100565b88077
roshanabey2 <roshanabey2@gmail.com>
2021-11-08 15:18:10 +0000

    Created app.rb, necessary gems and spec and with feature tests folder within that

---
+++ app.rb
@@ -0,0 +1,15 @@
+require "bundler/setup"
+require 'sinatra/base'
+require 'sinatra/reloader'
+
+class Bookmarks < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  get '/' do
+    'Hello World!'
+  end
+  # start the server if ruby file executed directly
+  run! if app_file == $0
+end
